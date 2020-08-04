ea6c0406b895b3f2bac4b7ecae507a863b284b92
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-08-04 20:41:07 -0300

    Fazendo listagem de classes e professres usando vários filtros com join e where

--- src/routes.ts
+++ src/routes.ts
@@ -1,60 +1,13 @@
 import express from 'express';
 import db from './database/connection';
-import convertHourToMinutes from './utils/convertHourToMinutos';
+
+import ClassesController from './controllers/ClassesController';
 
 const routes = express.Router();
+const classesControllers = new ClassesController();
 
-interface ScheduleItem {
-    week_day: number,
-    from: string,
-    to: string
-}
-
-routes.post('/classes', async (request, response) => {
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = request.body;
-
-    const insertedUsersIds = await db('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio,
-    });
-
-    const user_id = insertedUsersIds[0];
-
-    const insertedClassesIds = await db('classes').insert({
-        subject,
-        cost,
-        user_id,
-    });
-
-    const class_id = insertedClassesIds[0];
-
-    const classSchedule = schedule.map( (item: ScheduleItem) => {
-
-        return {
-            class_id,
-            week_day: item.week_day,
-            from: convertHourToMinutes(item.from),
-            to: convertHourToMinutes(item.to)
-        }
-    });
-
-    await db('class_schedule').insert(classSchedule);
-});
-
-routes.get("/classes", async (request, response) => {
-    const classes = await db('classes').select('*');
-    console.log(classes);
-});
+routes.post('/classes', classesControllers.create);
+routes.get('/classes', classesControllers.index);
 
 routes.get("/schedule", async (request, response) => {
     const classes = await db('class_schedule').select('*');
